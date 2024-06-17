874f1e28e5d62cc245500e5c7b8ebd5fe84786fd
Egor <etkachev398@gmail.com>
2024-06-17 18:26:49 +0300

    Separated training and testing, added requirements

---
+++ emotion_recognition/test.py
@@ -0,0 +1,38 @@
+from keras.src.saving import load_model
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+emotions_labels = {
+    0: 'Angry',
+    1: 'Disgust',
+    2: 'Fear',
+    3: 'Happy',
+    4: 'Sad',
+    5: 'Surprise',
+    6: 'Neutral'
+}
+
+
+def load_test_data(path):
+    data = pd.read_csv(path, nrows=15)
+    pixels = data['pixels'].apply(lambda image_pixels: np.fromstring(image_pixels, sep=' ').reshape(48, 48, 1))
+    emotions = data['emotion']
+    return np.array(pixels.tolist()), np.array(emotions.to_list())
+
+
+def main():
+    test_images_pixels, true_classes = load_test_data('../data/fer2013/fer2013.csv')
+    test_images_pixels = np.repeat(test_images_pixels, 3, axis=-1)
+    model = load_model('model.keras')
+    predictions = model.predict(test_images_pixels)
+    predicted_classes = np.argmax(predictions, axis=1)
+    for i in range(len(predicted_classes)):
+        print(f'Test case {i + 1}: {emotions_labels[predicted_classes[i]]}, label number: {predicted_classes[i]}')
+
+    accuracy = accuracy_score(true_classes, predicted_classes)
+    print(f'Accuracy: {accuracy}')
+
+
+if __name__ == '__main__':
+    main()
--- emotion_recognition/train.py
+++ emotion_recognition/train.py
@@ -7,33 +7,17 @@ from keras.src.legacy.preprocessing.image import ImageDataGenerator
 from keras.src.optimizers import Adam
 
 
-emotions_labels = {
-    0: 'Angry',
-    1: 'Disgust',
-    2: 'Fear',
-    3: 'Happy',
-    4: 'Sad',
-    5: 'Surprise',
-    6: 'Neutral'
-}
-
-
-def load_data(path, is_train):
+def load_train_data(path):
     data = pd.read_csv(path)
     pixels = data['pixels'].apply(lambda image_pixels: np.fromstring(image_pixels, sep=' ').reshape(48, 48, 1))
-    if is_train:
-        emotions = data['emotion']
-        return np.array(pixels.tolist()), np.array(emotions.tolist())
-
-    return np.array(pixels.tolist())
+    emotions = data['emotion']
+    return np.array(pixels.tolist()), np.array(emotions.tolist())
 
 
 def main():
-    # load train and test data
-    train_images_pixels, train_emotions = load_data('../data/train.csv', True)
-    test_images_pixels = load_data('../data/test.csv', False)
+    # load train data
+    train_images_pixels, train_emotions = load_train_data('../data/train.csv')
     train_images_pixels = np.repeat(train_images_pixels, 3, axis=-1)
-    test_images_pixels = np.repeat(test_images_pixels, 3, axis=-1)
     # augmentation
     datagen = ImageDataGenerator(
         rotation_range=10,
@@ -53,13 +37,9 @@ def main():
     output = Dense(7, activation='softmax')(x)
     model = Model(inputs=inputs, outputs=output)
     model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    # model.fit(datagen.flow(train_images_pixels, train_emotions, batch_size=64), epochs=15, validation_data=None)
+    model.fit(datagen.flow(train_images_pixels, train_emotions, batch_size=64), epochs=15, validation_data=None)
     # model.save_weights('model64_15.weights.h5')
-    model.load_weights('model64_15.weights.h5')
-    predictions = model.predict(test_images_pixels)
-    predicted_classes = np.argmax(predictions, axis=1)
-    for i in range(100):
-        print(f'Test case {i + 1}: {emotions_labels[predicted_classes[i]]}, label number: {predicted_classes[i]}')
+    model.save('model.keras')
 
 
 if __name__ == '__main__':
