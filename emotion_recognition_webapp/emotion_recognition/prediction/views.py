03ae85d79a9f27096912b5ae248bf381fb78fb8c
Egor <etkachev398@gmail.com>
2024-06-27 19:15:46 +0300

    Added check if prediction for file was done earlier

--- emotion_recognition_webapp/emotion_recognition/prediction/views.py
+++ emotion_recognition_webapp/emotion_recognition/prediction/views.py
@@ -9,16 +9,6 @@ import os
 from prediction.forms import ChooseFileForm
 from prediction.models import Prediction
 
-emotions_labels = {
-    0: 'angry',
-    1: 'disgust',
-    2: 'fear',
-    3: 'happy',
-    4: 'sad',
-    5: 'surprise',
-    6: 'neutral'
-}
-
 
 class ChooseFileView(View):
     template_name = 'prediction/index.html'
@@ -38,8 +28,7 @@ class ChooseFileView(View):
         return redirect('predict_emotions', prediction.id)
 
 
-def predict_emotions(pk):
-    prediction = Prediction.objects.get(pk=pk)
+def predict_emotions(prediction):
     data = read_csv(prediction.source_file)
     images_pixels = np.array(data['pixels'].apply(
         lambda image_pixels: np.fromstring(image_pixels, sep=' ').reshape(48, 48, 1)).tolist())
@@ -57,23 +46,40 @@ def predict_emotions(pk):
     prediction.save()
     os.makedirs('results', exist_ok=True)
     result_data.to_csv(result_path, index=False)
-    return [emotions_labels[predicted_class] for predicted_class in predicted_classes]
+    return predicted_classes
 
 
 class PredictEmotionsView(View):
     template_name = 'prediction/predict_emotions.html'
+    emotions_labels = {
+        0: 'angry',
+        1: 'disgust',
+        2: 'fear',
+        3: 'happy',
+        4: 'sad',
+        5: 'surprise',
+        6: 'neutral'
+    }
 
     def get(self, request, pk):
-        emotions = predict_emotions(pk)
-        request.user.operations_done += 1
-        request.user.save()
+        prediction = Prediction.objects.get(pk=pk)
+        if prediction.result_file == '':
+            predicted_classes = predict_emotions(prediction)
+            request.user.operations_done += 1
+            request.user.save()
+        else:
+            data = read_csv(prediction.result_file)
+            predicted_classes = data['emotion']
+
+        predicted_classes = [self.emotions_labels[predicted_class] for predicted_class in predicted_classes]
         return render(request, self.template_name, {
-            'emotions': emotions,
+            'emotions': predicted_classes,
             'prediction_pk': pk
         })
 
 
 class DownloadPredictionView(View):
 
-    def get(self, request, pk):
+    @staticmethod
+    def get(request, pk):
         return FileResponse(Prediction.objects.get(pk=pk).result_file, as_attachment=True)
