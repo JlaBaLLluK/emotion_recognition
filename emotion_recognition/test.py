6bafb7dbdde15266cf34ca172474052114403f12
Egor <etkachev398@gmail.com>
2024-07-02 15:27:32 +0300

    Added prediction from image file

--- emotion_recognition/test.py
+++ emotion_recognition/test.py
@@ -2,6 +2,7 @@ from keras.src.saving import load_model
 import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score
+import cv2
 
 emotions_labels = {
     0: 'Angry',
@@ -18,20 +19,34 @@ def load_test_data(path):
     data = pd.read_csv(path)
     pixels = data['pixels'].apply(lambda image_pixels: np.fromstring(image_pixels, sep=' ').reshape(48, 48, 1))
     emotions = data['emotion']
-    return np.array(pixels.tolist()), np.array(emotions.to_list())
+    pixels = np.array(pixels.tolist())
+    for i in range(100):
+        cv2.imwrite(f'image_{i}.png', pixels[i])
+    # return np.array(pixels.tolist()), np.array(emotions.to_list())
+
+
+def load_image(path):
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        print("Error while load image!")
+        return
+
+    if img.shape[0] != 48 or img.shape[1] != 48:
+        img = cv2.resize(img, (48, 48))
+
+    img = img.astype('float32')
+    img = np.repeat(img[..., np.newaxis], 3, axis=-1)
+    img = np.expand_dims(img, axis=0)
+    return img
 
 
 def main():
-    test_images_pixels, true_classes = load_test_data('../data/fer2013/fer2013.csv')
-    test_images_pixels = np.repeat(test_images_pixels, 3, axis=-1)
-    model = load_model('model.keras')
-    predictions = model.predict(test_images_pixels)
+    test_image_pixels = load_image('../data/imgs/image_7.png')
+    model = load_model('../model.keras')
+    predictions = model.predict(test_image_pixels)
     predicted_classes = np.argmax(predictions, axis=1)
     for i in range(len(predicted_classes)):
         print(f'Test case {i + 1}: {emotions_labels[predicted_classes[i]]}, label number: {predicted_classes[i]}')
-
-    accuracy = accuracy_score(true_classes, predicted_classes)
-    print(f'Accuracy: {accuracy}')
 
 
 if __name__ == '__main__':
